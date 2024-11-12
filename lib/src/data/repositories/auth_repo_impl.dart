34b1d6c2fe64f7c6e91c68a5f1439b2261cd65b9
ali72-20 <as20201118@gmail.com>
2024-11-12 19:01:57 +0200

    add: either packages add: Auth repo getLoggedUserData method add: signIn User method

--- lib/src/data/repositories/auth_repo_impl.dart
+++ lib/src/data/repositories/auth_repo_impl.dart
@@ -1,7 +1,27 @@
+import 'package:either_dart/src/either.dart';
+import 'package:flower_app/src/data/data_sources/offline_data_source/offline_data_source.dart';
+import 'package:flower_app/src/data/data_sources/online_data_source/online_data_source.dart';
+import 'package:flower_app/src/domain/entities/user_entity.dart';
 import 'package:flower_app/src/domain/repositories/auth_repo.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable(as: AuthRepository)
 class AuthRepositoryImpl implements  AuthRepository{
+  OnlineDataSource _onlineDataSource;
+  OfflineDataSource _offlineDataSource;
+  AuthRepositoryImpl(this._offlineDataSource,this._onlineDataSource);
+
+  @override
+  Future<Either<String, UserEntity>> getLoggedUserData() {
+    // TODO: implement getLoggedUserData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
 
 }
--- lib/src/domain/repositories/auth_repo.dart
+++ lib/src/domain/repositories/auth_repo.dart
@@ -1,3 +1,7 @@
-abstract class AuthRepository{
+import 'package:either_dart/either.dart';
+import 'package:flower_app/src/domain/entities/user_entity.dart';
 
+abstract class AuthRepository{
+  Future<void> signIn(String email, String password);
+  Future<Either<String,UserEntity>> getLoggedUserData();
 }
