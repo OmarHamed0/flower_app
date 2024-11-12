1c0534948586b0a486d24c8bf52abaef425464b0
ali72-20 <as20201118@gmail.com>
2024-11-12 17:54:45 +0200

    add: online data source call

--- lib/src/data/data_sources/online_data_source/online_data_source.dart
+++ lib/src/data/data_sources/online_data_source/online_data_source.dart
@@ -1,3 +1,7 @@
-abstract class OnlineDataSource{
+import 'package:flower_app/src/data/api/core/response_model/signin_response_model.dart';
+import 'package:flower_app/src/data/models/usr_model_dto.dart';
 
+abstract class OnlineDataSource{
+  Future<SignInResponseModel> signin(String email,String password);
+  Future<UserModelDTO> getLoggedUserData();
 }
--- lib/src/domain/entities/user_entity.dart
+++ lib/src/domain/entities/user_entity.dart
@@ -18,5 +18,4 @@ class UserEntity {
     this.photo,
     this.role
   });
-
 }
