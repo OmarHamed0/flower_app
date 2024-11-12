a5b6dadfef883bb02de5cd1dc9ed625e305d0236
ali72-20 <as20201118@gmail.com>
2024-11-12 17:54:45 +0200

    add: online data source call

--- lib/src/data/data_sources/online_data_source/online_data_source_impl.dart
+++ lib/src/data/data_sources/online_data_source/online_data_source_impl.dart
@@ -1,10 +1,25 @@
 import 'package:flower_app/src/data/api/api_services.dart';
+import 'package:flower_app/src/data/api/core/requestes_models/signin_request_body.dart';
+import 'package:flower_app/src/data/api/core/response_model/signin_response_model.dart';
 import 'package:flower_app/src/data/data_sources/online_data_source/online_data_source.dart';
+import 'package:flower_app/src/data/models/usr_model_dto.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable(as: OnlineDataSource)
-class OnlineDataSourceImpl implements OnlineDataSource{
+class OnlineDataSourceImpl implements OnlineDataSource {
   ApiServices _apiServices;
+
   OnlineDataSourceImpl(this._apiServices);
 
+  @override
+  Future<SignInResponseModel> signin(String email, String password) {
+    return _apiServices
+        .signIn(SignInRequestBody(email: email, password: password));
+  }
+
+  @override
+  Future<UserModelDTO> getLoggedUserData() async {
+    var response = await _apiServices.getLoggedUserData("");
+    return UserModelDTO.fromResponse(response);
+  }
 }
