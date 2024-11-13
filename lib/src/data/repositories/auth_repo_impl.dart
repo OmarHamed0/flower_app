0db4f8afe56d2f95a320ae19ea53b9e13a47be35
ali72-20 <as20201118@gmail.com>
2024-11-14 00:48:52 +0200

    update: auth repo update: auth use case

--- lib/src/data/repositories/auth_repo_impl.dart
+++ lib/src/data/repositories/auth_repo_impl.dart
@@ -1,15 +1,20 @@
 import 'package:either_dart/src/either.dart';
+import 'package:flower_app/common/api_result.dart';
+import 'package:flower_app/src/data/api/core/response_model/signin_response_model.dart';
 import 'package:flower_app/src/data/data_sources/offline_data_source/offline_data_source.dart';
 import 'package:flower_app/src/data/data_sources/online_data_source/online_data_source.dart';
+import 'package:flower_app/src/data/models/sign_in_response_dto.dart';
+import 'package:flower_app/src/domain/entities/sign_in_entity.dart';
 import 'package:flower_app/src/domain/entities/user_entity.dart';
 import 'package:flower_app/src/domain/repositories/auth_repo.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable(as: AuthRepository)
-class AuthRepositoryImpl implements  AuthRepository{
+class AuthRepositoryImpl implements AuthRepository {
   OnlineDataSource _onlineDataSource;
   OfflineDataSource _offlineDataSource;
-  AuthRepositoryImpl(this._offlineDataSource,this._onlineDataSource);
+
+  AuthRepositoryImpl(this._offlineDataSource, this._onlineDataSource);
 
   @override
   Future<Either<String, UserEntity>> getLoggedUserData() {
@@ -18,10 +23,16 @@ class AuthRepositoryImpl implements  AuthRepository{
   }
 
   @override
-  Future<void> signIn(String email, String password) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<ApiResult<SignInEntity>> signIn(String email, String password) async {
+    try {
+      var response = await _onlineDataSource.signIn(email, password);
+      Success<SignInResponseModel>? success;
+      if(response is Success<SignInResponseModel>){
+        success = response;
+      }
+      return Success(data: SignInResponseDto.toDomain(success!));
+    }on Exception catch(e){
+      return Failures(exception: e);
+    }
   }
-
-
 }
--- lib/src/domain/repositories/auth_repo.dart
+++ lib/src/domain/repositories/auth_repo.dart
@@ -1,7 +1,9 @@
 import 'package:either_dart/either.dart';
+import 'package:flower_app/common/api_result.dart';
 import 'package:flower_app/src/domain/entities/user_entity.dart';
+import '../entities/sign_in_entity.dart';
 
 abstract class AuthRepository{
-  Future<void> signIn(String email, String password);
+  Future<ApiResult<SignInEntity>> signIn(String email, String password);
   Future<Either<String,UserEntity>> getLoggedUserData();
 }
