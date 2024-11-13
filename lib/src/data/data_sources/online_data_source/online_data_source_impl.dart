51dec553b4a5291d891308f14c40de908b905c73
ali72-20 <as20201118@gmail.com>
2024-11-14 00:48:29 +0200

    update: online data Source

--- lib/src/data/data_sources/online_data_source/online_data_source_impl.dart
+++ lib/src/data/data_sources/online_data_source/online_data_source_impl.dart
@@ -1,9 +1,12 @@
+import 'package:flower_app/common/api_execute.dart';
+import 'package:flower_app/common/api_result.dart';
 import 'package:flower_app/config/helpers/shared_pre_keys.dart';
 import 'package:flower_app/config/helpers/shared_pref_helper.dart';
 import 'package:flower_app/src/data/api/api_services.dart';
 import 'package:flower_app/src/data/api/core/requestes_models/signin_request_body.dart';
 import 'package:flower_app/src/data/api/core/response_model/signin_response_model.dart';
 import 'package:flower_app/src/data/data_sources/online_data_source/online_data_source.dart';
+import 'package:flower_app/src/data/models/sign_in_response_dto.dart';
 import 'package:flower_app/src/data/models/usr_model_dto.dart';
 import 'package:injectable/injectable.dart';
 
@@ -14,16 +17,25 @@ class OnlineDataSourceImpl implements OnlineDataSource {
   OnlineDataSourceImpl(this._apiServices);
 
   @override
-  Future<SignInResponseModel> signin(String email, String password)async {
-    var response = await _apiServices.signIn(SignInRequestBody(email: email, password: password));
-    ShardPrefHelper.setSecureString(SharedPrefKeys.userToken, response.token??"");
-    return response;
-  }
-
-  @override
   Future<UserModelDTO> getLoggedUserData() async {
     String token = ShardPrefHelper.getSecureString(SharedPrefKeys.userToken);
     var response = await _apiServices.getLoggedUserData(token);
     return UserModelDTO.fromResponse(response);
+  }
+
+  @override
+  Future<ApiResult<SignInResponseModel>> signIn(
+      String email, String password) async {
+    var response = await executeApi<SignInResponseModel>(apiCall: () async {
+      return await _apiServices
+          .signIn(SignInRequestBody(email: email, password: password));
+    });
+    switch (response) {
+      case Success<SignInResponseModel>():
+        ShardPrefHelper.setSecureString(
+            SharedPrefKeys.userToken, response.data!.token ?? "");
+      case Failures<SignInResponseModel>():
+    }
+    return response;
   }
 }
