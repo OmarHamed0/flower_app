d0a62e7ea904d3b484bc98f3f23c45f496ef40a1
ali72-20 <as20201118@gmail.com>
2024-11-14 00:48:29 +0200

    update: online data Source

--- lib/src/data/data_sources/online_data_source/online_data_source.dart
+++ lib/src/data/data_sources/online_data_source/online_data_source.dart
@@ -1,7 +1,8 @@
+import 'package:flower_app/common/api_result.dart';
 import 'package:flower_app/src/data/api/core/response_model/signin_response_model.dart';
 import 'package:flower_app/src/data/models/usr_model_dto.dart';
 
 abstract class OnlineDataSource{
-  Future<SignInResponseModel> signin(String email,String password);
+  Future<ApiResult<SignInResponseModel>> signIn(String email,String password);
   Future<UserModelDTO> getLoggedUserData();
 }
