fb146628931a9a97ca3c03062dbb694530887f5a
ali72-20 <as20201118@gmail.com>
2024-11-15 00:43:45 +0200

    add: sign in form field

---
+++ lib/src/presentation/pages/sign_in/remember_me_forget_password_row.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/styles/texts/app_text_styles.dart';
+import '../../managers/sign_in/sign_in_view_model.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class RememberMeForgetPasswordRow extends StatelessWidget {
+  const RememberMeForgetPasswordRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final signInViewModel = context.read<SignInViewModel>();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Checkbox(
+              value: signInViewModel.isBoxChecked,
+              onChanged: (value) {
+                signInViewModel.isBoxChecked = value!;
+              },
+            ),
+            Text(
+              AppLocalizations.of(context)!.rememberMe,
+              style: AppTextStyles.font13WeightNormal,
+            )
+          ],
+        ),
+        Text(
+          AppLocalizations.of(context)!.forgetPassword,
+          style: AppTextStyles.font13WeightNormal
+              .copyWith(decoration: TextDecoration.underline),
+        )
+      ],
+    );
+  }
+}
