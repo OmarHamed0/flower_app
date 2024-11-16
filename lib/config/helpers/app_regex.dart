62868b197c7c093da9b9900568fd5a8ca1546757
ali72-20 <as20201118@gmail.com>
2024-11-16 19:01:11 +0200

    add: app_regex add: update text field

---
+++ lib/config/helpers/app_regex.dart
@@ -0,0 +1,6 @@
+class AppRegex{
+  AppRegex._();
+  static final RegExp emailValidationRegex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+  static final RegExp passwordValidationRegex = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$');
+
+}
