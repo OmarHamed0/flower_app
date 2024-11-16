e13ebc473b69304801ed5627e6d8e460c8b59e87
ali72-20 <as20201118@gmail.com>
2024-11-16 22:41:43 +0200

    add: error lottie add: error dialog animation update: Sign in screen body (Show error dialog)

--- lib/core/animations/app_animation.dart
+++ lib/core/animations/app_animation.dart
@@ -2,4 +2,5 @@ class AppAnimations{
   AppAnimations._();
   static String loading = "assets/animations/eYr0JZUKw0.json";
   static String success = "assets/animations/success.json";
+  static String error = "assets/animations/error.json";
 }
--- lib/src/presentation/widgets/dialogs.dart
+++ lib/src/presentation/widgets/dialogs.dart
@@ -33,9 +33,6 @@ class LoadingDialog {
   }
 }
 
-
-
-
 class SuccessDialog {
   static void show(BuildContext context) {
     showDialog(
@@ -52,6 +49,37 @@ class SuccessDialog {
             AppAnimations.success,
             width: 200,
             height: 200,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
+
+  static void hide(BuildContext context) {
+    // Pop the dialog if it is still displayed
+    if (Navigator.of(context).canPop()) {
+      Navigator.of(context).pop();
+    }
+  }
+}
+
+class ErrorDialog{
+  static void show(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      // Prevent dismissing the dialog by tapping outside
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10), // Rounded corners
+          ),
+          content: Lottie.asset(
+            AppAnimations.error,
+            width: 300,
+            height: 300,
             fit: BoxFit.cover,
           ),
         );
