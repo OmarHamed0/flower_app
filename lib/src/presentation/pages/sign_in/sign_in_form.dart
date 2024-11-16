a7aef758ebe60bf8e9c77460e5ae4e001e0a47c4
ali72-20 <as20201118@gmail.com>
2024-11-16 19:01:39 +0200

    add: validate email and password

--- lib/src/presentation/pages/sign_in/sign_in_form.dart
+++ lib/src/presentation/pages/sign_in/sign_in_form.dart
@@ -1,3 +1,4 @@
+import 'package:flower_app/core/styles/colors/app_colors.dart';
 import 'package:flower_app/core/styles/spaceing.dart';
 import 'package:flower_app/src/presentation/managers/sign_in/sign_in_actions.dart';
 import 'package:flower_app/src/presentation/managers/sign_in/sign_in_states.dart';
@@ -6,54 +7,67 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/styles/texts/app_text_styles.dart';
 import '../../widgets/app_text_field.dart';
 
-class SignInForm extends StatefulWidget {
+class SignInForm extends StatelessWidget {
   const SignInForm({super.key});
 
-  @override
-  State<SignInForm> createState() => _SignInFormState();
-}
-
-class _SignInFormState extends State<SignInForm> {
   @override
   Widget build(BuildContext context) {
     final signInViewModel = context.read<SignInViewModel>();
     IconData visibility = Icons.visibility_off;
+    String? emailErrorMessage;
+    String? passwordErrorMessage;
     return Form(
+      key: signInViewModel.signInFormKey,
       child: Column(
         children: [
           AppTextField(
             controller: signInViewModel.emailController,
             labelText: AppLocalizations.of(context)!.email,
             hintText: AppLocalizations.of(context)!.enterYourEmail,
-            labelStyle: AppTextStyles.font12WeightNormal,
+            labelStyle: AppTextStyles.font12WeightNormal.copyWith(
+                color: emailErrorMessage != null
+                    ? AppColors.kError
+                    : AppColors.kGray),
             hintStyle: AppTextStyles.font14WeightNormal,
+            errorText: emailErrorMessage,
+            validator: (value) {
+              emailErrorMessage = signInViewModel.validateEmail();
+              return emailErrorMessage;
+            },
           ),
           verticalSpace(14),
-          BlocBuilder<SignInViewModel,SignInStates>(
+          BlocBuilder<SignInViewModel, SignInStates>(
             builder: (context, state) {
-              if(state is VisiblePasswordState){
-                visibility= Icons.visibility;
-              }else if (state is NonVisiblePasswordState){
+              if (state is VisiblePasswordState) {
+                visibility = Icons.visibility;
+              } else if (state is NonVisiblePasswordState) {
                 visibility = Icons.visibility_off;
               }
               return AppTextField(
-                controller: signInViewModel.passwordController,
-                labelText: AppLocalizations.of(context)!.password,
-                hintText: AppLocalizations.of(context)!.enterYourPassword,
-                hintStyle: AppTextStyles.font14WeightNormal,
-                labelStyle: AppTextStyles.font12WeightNormal,
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    signInViewModel.doAction(ChangePasswordVisibilityAction());
-                  },
-                  icon: Icon(visibility),
-                ),
-                obscureText: signInViewModel.isObscureText,
-              );
+                  controller: signInViewModel.passwordController,
+                  labelText: AppLocalizations.of(context)!.password,
+                  hintText: AppLocalizations.of(context)!.enterYourPassword,
+                  hintStyle: AppTextStyles.font14WeightNormal,
+                  labelStyle: AppTextStyles.font12WeightNormal.copyWith(
+                      color: passwordErrorMessage != null
+                          ? AppColors.kError
+                          : AppColors.kGray),
+                  errorText: passwordErrorMessage,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      signInViewModel
+                          .doAction(ChangePasswordVisibilityAction());
+                    },
+                    icon: Icon(visibility),
+                  ),
+                  obscureText: signInViewModel.isObscureText,
+                  validator: (value) {
+                    passwordErrorMessage = signInViewModel.validatePassword();
+                    return passwordErrorMessage;
+                  });
             },
           ),
         ],
