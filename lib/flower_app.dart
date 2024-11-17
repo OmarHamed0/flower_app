ca748e2b659b3a8b35f15258a7385a6e4152d955
ali72-20 <as20201118@gmail.com>
2024-11-17 02:19:24 +0200

    add: auto login

--- lib/flower_app.dart
+++ lib/flower_app.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
 
+import 'package:flower_app/config/helpers/shared_pre_keys.dart';
+import 'package:flower_app/config/helpers/shared_pref_helper.dart';
 import 'package:flower_app/config/localization/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -8,12 +11,29 @@ import 'config/routes/page_route_name.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 final GlobalKey<NavigatorState> navKey = GlobalKey<NavigatorState>();
+
 class FlowerApp extends StatelessWidget {
   const FlowerApp({super.key});
 
+  Future<void> _navigateBasedOnToken() async {
+    // Fetch the user token from secure storage
+    final userToken = await SharedPrefHelper.getSecureString(SharedPrefKeys.userToken);
+    log("User token: $userToken");
+
+    // Navigate based on token
+    final initialRoute = (userToken != null && userToken.isNotEmpty)
+        ? PageRouteName.home
+        : PageRouteName.signIn;
+
+    navKey.currentState?.pushReplacementNamed(initialRoute);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
+    // Start the navigation process
+    WidgetsBinding.instance.addPostFrameCallback((_) => _navigateBasedOnToken());
+
+    return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
@@ -22,16 +42,16 @@ class FlowerApp extends StatelessWidget {
           AppLocalizations.delegate,
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate
+          GlobalCupertinoLocalizations.delegate,
         ],
         supportedLocales: L10n.all,
-        debugShowCheckedModeBanner:  false,
+        debugShowCheckedModeBanner: false,
         locale: const Locale('en'),
         navigatorKey: navKey,
-        initialRoute: PageRouteName.signIn,
+        initialRoute: PageRouteName.splash, // Temporary splash screen
         onGenerateRoute: AppRoute.onGenerateRoute,
-        themeMode:ThemeMode.light,
-       // theme: AppTheme.appTheme,s
+        themeMode: ThemeMode.light,
+        // theme: AppTheme.appTheme,
       ),
     );
   }
