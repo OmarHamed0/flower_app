76f2849b6d982a5fc65f44e9fb04ea63e8517fa7
ali72-20 <as20201118@gmail.com>
2024-11-22 05:24:01 +0200

    add: product Images slider

--- lib/src/presentation/pages/product_details/product_details_body.dart
+++ lib/src/presentation/pages/product_details/product_details_body.dart
@@ -1,6 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flower_app/config/routes/app_route.dart';
 import 'package:flower_app/core/styles/app_radius.dart';
 import 'package:flower_app/src/presentation/managers/product_details/product_details_view_model.dart';
+import 'package:flower_app/src/presentation/pages/product_details/images_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flower_app/core/styles/colors/app_colors.dart';
 import 'package:flower_app/core/styles/images/app_images.dart';
@@ -30,18 +32,16 @@ class ProductDetailsBody extends StatelessWidget {
             flexibleSpace: LayoutBuilder(
               builder: (context, constraints) {
                 double top = constraints.biggest.height;
-                bool isCollapsed = top <= kToolbarHeight + MediaQuery.of(context).padding.top;
+                bool isCollapsed =
+                    top <= kToolbarHeight + MediaQuery.of(context).padding.top;
                 return FlexibleSpaceBar(
                   title: isCollapsed
                       ? Text(
-                    productEntity?.title ?? "",
-                    style: AppTextStyles.font20WeightBold,
-                  )
+                          productEntity?.title ?? "",
+                          style: AppTextStyles.font20WeightBold,
+                        )
                       : null,
-                  background: Image.network(
-                    productEntity!.imageCover!,
-                    fit: BoxFit.cover,
-                  ),
+                  background: const ImagesSlider(),
                   centerTitle: true,
                 );
               },
@@ -58,7 +58,8 @@ class ProductDetailsBody extends StatelessWidget {
                   verticalSpace(4),
                   Text(
                     "All prices include Tax",
-                    style: AppTextStyles.font13WeightNormal.copyWith(color: AppColors.kGray),
+                    style: AppTextStyles.font13WeightNormal
+                        .copyWith(color: AppColors.kGray),
                   ),
                   verticalSpace(4),
                   Text(
@@ -73,26 +74,31 @@ class ProductDetailsBody extends StatelessWidget {
                   verticalSpace(8),
                   Text(
                     productEntity?.description ?? "",
-                    style: AppTextStyles.font14WeightNormal.copyWith(color: AppColors.kBlack),
+                    style: AppTextStyles.font14WeightNormal
+                        .copyWith(color: AppColors.kBlack),
                   ),
                   verticalSpace(24),
-                  Text("Bouquet include", style: AppTextStyles.font16WeightMedium),
+                  Text("Bouquet include",
+                      style: AppTextStyles.font16WeightMedium),
                   verticalSpace(8),
                   Text(
                     "pink roses: 15",
-                    style: AppTextStyles.font14WeightNormal.copyWith(color: AppColors.kBlack),
+                    style: AppTextStyles.font14WeightNormal
+                        .copyWith(color: AppColors.kBlack),
                   ),
                   verticalSpace(4),
                   Text(
                     "White wrap",
-                    style: AppTextStyles.font14WeightNormal.copyWith(color: AppColors.kBlack),
+                    style: AppTextStyles.font14WeightNormal
+                        .copyWith(color: AppColors.kBlack),
                   ),
                   verticalSpace(24),
                   FadeInUp(
                     child: AppTextButton(
                       borderRadius: AppBorderRadius.xxl,
                       buttonText: "Add to cart",
-                      textStyle: AppTextStyles.font16WeightMedium.copyWith(color: AppColors.kWhiteBase),
+                      textStyle: AppTextStyles.font16WeightMedium
+                          .copyWith(color: AppColors.kWhiteBase),
                       onPressed: () {},
                     ),
                   ),
