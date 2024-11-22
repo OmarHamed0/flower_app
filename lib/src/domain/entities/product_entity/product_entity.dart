b28868a65f129be19934a570b5d7c73cb96d260c
ali72-20 <as20201118@gmail.com>
2024-11-22 03:35:33 +0200

    update:product quantity

--- lib/src/domain/entities/product_entity/product_entity.dart
+++ lib/src/domain/entities/product_entity/product_entity.dart
@@ -1,9 +1,23 @@
-class ProductEntity{
+class ProductEntity {
   String? id;
   String? title;
   String? description;
+  String? slug;
+  String? imageCover;
+  List<String>? images;
+  int? price;
+  int? priceAfterDiscount;
+  int? quantity;
 
-
-
-  ProductEntity({this.id, this.title,this.description});
+  ProductEntity({
+    this.id,
+    this.title,
+    this.description,
+    this.slug,
+    this.imageCover,
+    this.images,
+    this.price,
+    this.priceAfterDiscount,
+    this.quantity
+  });
 }
