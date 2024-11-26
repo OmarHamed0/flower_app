4bcac8d389300ef186d6042bfcd5894a529dc087
ali72-20 <as20201118@gmail.com>
2024-11-26 18:27:57 +0200

    add: get categories api call

--- lib/src/domain/repositories/product_repo/product_repo.dart
+++ lib/src/domain/repositories/product_repo/product_repo.dart
@@ -1,4 +1,5 @@
 import 'package:flower_app/common/api_result.dart';
+import 'package:flower_app/src/domain/entities/category_entity/category_entity.dart';
 import 'package:flower_app/src/domain/entities/product_entity/product_entity.dart';
 
 abstract interface class ProductRepo{
