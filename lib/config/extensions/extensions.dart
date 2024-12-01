aaadf1e35070ed1a1a9ada8724e5bdf8be6431d0
ali72-20 <as20201118@gmail.com>
2024-12-01 23:08:16 +0200

    add: context extension to get height and width of context

--- lib/config/extensions/extensions.dart
+++ lib/config/extensions/extensions.dart
@@ -1,4 +1,17 @@
+import 'package:flower_app/common/common.dart';
+
 extension StringExtenstion on String?{
   /// check this string [value] is empty or null
   bool isNullOrEmpty()=> this == null || this == "";
+}
+
+/// Context Extension to get height and width of the screen
+/// usage: context.height, context.width
+extension ContextExtension on BuildContext {
+  /// Get height of context screen
+  /// usage: context.height
+  get height => MediaQuery.of(this).size.height;
+  /// Get width of context screen
+  /// usage: context.width
+  get width => MediaQuery.of(this).size.width;
 }
---
+++ lib/src/presentation/pages/reset_passowrd/reset_password_screen.dart
@@ -0,0 +1,10 @@
+
+import 'package:flower_app/common/common.dart';
+class ResetPasswordScreen extends StatelessWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
