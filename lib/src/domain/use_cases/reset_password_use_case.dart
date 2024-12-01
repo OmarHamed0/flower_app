fb33759056a307937f7c4374abc02c5898177fe0
ali72-20 <as20201118@gmail.com>
2024-12-01 04:46:36 +0200

    add: reset  password entity add: reset password use case

---
+++ lib/src/domain/use_cases/reset_password_use_case.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flower_app/common/api_result.dart';
+import 'package:flower_app/src/domain/entities/auth/reset_password_entity.dart';
+import 'package:flower_app/src/domain/repositories/auth_repo.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class ResetPasswordUseCase{
+  final  AuthRepository _authRepository;
+  ResetPasswordUseCase(this._authRepository);
+
+  Future<ApiResult<ResetPasswordEntity>> resetPassword(String oldPassword,String newPassword) async {
+    return await _authRepository.resetPassword(oldPassword, newPassword);
+  }
+}
